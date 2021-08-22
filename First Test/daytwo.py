62aee0025cbe08e03b3d2c8eeca35d113d490f8f
Thomas Lo <thomasthepie@gmail.com>
2021-08-21 20:11:08 -0700

    learn about for loops and lists

---
+++ First Test/daytwo.py
@@ -0,0 +1,34 @@
+#Review
+print('Hello World')
+
+string = "this is a string"
+the = 15
+flt = 3.0
+print(the)
+
+
+#New Stuff
+print(type(flt))
+
+lst = [1, 3, 5, "string", 3.0]
+print(lst[3])
+
+#for loop
+for legallegends in lst:
+    print(legallegends)
+
+for watch in range(11):
+    print(watch)
+
+lst_two = [3, 4, 5, 6]
+scores = [66, 76, 89, 92, 34]
+
+for num in lst_two:
+    print(num)
+
+for score in scores:
+    print(score)
+
+#indexing
+new_lst = [2, 3, 1, "j", 4, 10]
+print(new_lst[1:4])
